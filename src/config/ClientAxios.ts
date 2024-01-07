995b7f185716491526fafd78f6b5ce093ab565d9
Martin Alexander Flores Torres <martinflorestorres21@gmail.com>
2024-01-06 19:52:58 -0500

    v1 prueba build

--- src/config/ClientAxios.ts
+++ src/config/ClientAxios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const ClientAxios = axios.create({
-  baseURL: 'http://localhost:4000/api/v1',
+  baseURL: `${import.meta.env.VITE_BACKEND_API_URL}/api/v1`,
 });
 
 export default ClientAxios;
